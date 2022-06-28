1e5a35e2e9ddb7f7af70575a333ee62f04498d0d
Ashish Pimpre <ashishpimpre.connect@gmail.com>
2022-06-28 22:31:47 +0530

    initial commit

---
+++ app/src/main/java/com/synerf/openstore/activities/SplashActivity.kt
@@ -0,0 +1,42 @@
+package com.synerf.openstore.activities
+
+import android.content.Intent
+import android.os.*
+import androidx.appcompat.app.AppCompatActivity
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.synerf.openstore.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // to hide status bar
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        // go to main activity after 2.5 seconds
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        },2500)
+
+//        // custom font
+//        val typeface: Typeface = Typeface.createFromAsset(assets, "Montserrat-Bold.ttf")
+//
+//        // set custom font to tv_app_name
+//        binding.tvAppName.typeface = typeface
+    }
+}
---
+++ app/src/main/java/com/synerf/openstore/utils/MSPTextViewBold.kt
@@ -0,0 +1,22 @@
+package com.synerf.openstore.utils
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+import java.util.jar.Attributes
+
+class MSPTextViewBold(context: Context, attrs: AttributeSet): AppCompatTextView(context, attrs) {
+    init {
+        // call the function to apply font to the components
+        applyFont();
+    }
+
+    // function to apply font
+    private fun applyFont() {
+        // get font file from asset folder and set it to the title textview
+        val typeFace: Typeface = Typeface.createFromAsset(context.assets, "Montserrat-Bold.ttf")
+
+        setTypeface(typeFace)
+    }
+}
